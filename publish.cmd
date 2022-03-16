fbd702d57e0cda3b9b3955a53d357ac03d12cbfe
Saternus <martin@saternus-consulting.de>
2022-03-16 12:34:13 +0100

    Initial Upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotAllFeatures.sln -p:DeployOnBuild=true -p:PublishProfile=.\westus-bot-ruk-plast-Web-Deploy.pubxml -p:Password=SmRaKDk82Ci2QYHZnG1NdMgJ0ozizJFHyXbfZFSyrm0YmajsvQqpXC0GENPo
+
